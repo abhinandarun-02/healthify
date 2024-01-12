651116a2a58d640aa7fc723d110e458a82a66492
Abhinand Arun <abhinandarun2015@gmail.com>
2024-01-12 19:11:45 +0530

    frontend(feat): Setup Shadcn UI

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,20 +1,96 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        border: "#e2e8f0", // Default border color
+        input: "#e2e8f0", // Border color for inputs such as <Input />, <Select />, <Textarea />
+        ring: "#2563eb", // Used for focus ring
+        
+        // Default background color of <body />...etc
+        background: "#ffffff",
+        foreground: "#020817",
+        
+        // Primary colors for <Button />
+        primary: {
+          DEFAULT: "#2563eb",
+          foreground: "#f8fafc",
+        },
+        
+        // Secondary colors for <Button />
+        secondary: {
+          DEFAULT: "#f1f5f9",
+          foreground: "#0f172a",
+        },
+
+        // Used for destructive actions such as <Button variant="destructive">
+        destructive: {
+          DEFAULT: "#ef4444",
+          foreground: "#f8fafc",
+        },
+
+        // Muted backgrounds such as <TabsList />, <Skeleton /> and <Switch />
+        muted: {
+          DEFAULT: "#f1f5f9",
+          foreground: "#64748b",
+        },
+
+        // Used for accents such as hover effects on <DropdownMenuItem>, <SelectItem>...etc
+        accent: {
+          DEFAULT: "#f1f5f9",
+          foreground: "#0f172a",
+        },
+
+        // Background color for popovers such as <DropdownMenu />, <HoverCard />, <Popover />
+        popover: {
+          DEFAULT: "#ffffff",
+          foreground: "#020817",
+        },
+
+        // Background color for <Card />
+        card: {
+          DEFAULT: "#ffffff",
+          foreground: "#020817",
+        },
+      },
+      borderRadius: {
+        lg: "0.5rem",
+        md: "calc(0.5rem - 2px)",
+        sm: "calc(0.5rem - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
 export default config
