4005945288c3652ee68c97f886ec472e32718fbb
Abhinand Arun <abhinandarun2015@gmail.com>
2024-01-16 20:59:52 +0530

    frontend(feat): Add pricing section

---
+++ frontend/app/[lang]/dashboard/chat/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>Stay Tuned.. Under Development</div>
+  )
+}
+
+export default Page
--- frontend/app/[lang]/page.tsx
+++ frontend/app/[lang]/page.tsx
@@ -3,14 +3,15 @@ import About from "@/components/marketing/about";
 import Hero from "@/components/marketing/hero";
 import NavBar from "@/components/shared/navbar/navbar";
 import Features from "@/components/marketing/features";
+import Pricing from "@/components/marketing/pricing";
 
 export default function Home() {
   return (
     <div className="py-20 px-24">
       <Hero />
       <Features />
+      <Pricing />
       <About />
     </div>
   );
 }
-  
